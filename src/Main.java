0fc4c991aea1ca8fd4dc96acf0238b89eaf9b56d
Sutter Yannick (s) <yannick.sutter@students.fhnw.ch>
2017-12-13 08:26:17 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    int erwarteteAnzahl;
+    int filtergrösse;
+    int hashfunktionen;
+
+    public static void main(String[] args) throws IOException {
+        double fehlerwahrscheinlichkeit = Double.parseDouble(args[0]);
+
+        FileReader fr = new FileReader("words.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        int counter = 0;
+        String line;
+        while ((line = br.readLine()) != null) {
+            counter++;
+            System.out.println(line);
+        }
+        //doppelt so gross wie anzahl elemente
+        int filtergrösse = 2*counter;
+        int [] array = new int[filtergrösse];
+        System.out.println(array.length);
+        br.close();
+
+
+
+
     }
 }
