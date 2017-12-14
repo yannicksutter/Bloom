dd278b668c999c80c174dbbed192ec3d0ca6cc09
Sutter Yannick (s) <yannick.sutter@students.fhnw.ch>
2017-12-14 12:56:43 +0100

    add words.txt

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.text.MessageFormat;
